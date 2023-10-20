e676ebdac8eb08f9e844fa98bba59923f0567800
Chengsong Zhang <continuerevolution@gmail.com>
2023-10-20 03:41:00 -0500

    support batch

--- groundingdino/util/inference.py
+++ groundingdino/util/inference.py
@@ -55,9 +55,7 @@ def predict(
         image: torch.Tensor,
         caption: str,
         box_threshold: float,
-        text_threshold: float,
         device: str = "cuda",
-        remove_combined: bool = False
 ) -> Tuple[torch.Tensor, torch.Tensor, List[str]]:
     caption = preprocess_caption(caption=caption)
 
@@ -65,36 +63,15 @@ def predict(
     image = image.to(device)
 
     with torch.no_grad():
-        outputs = model(image[None], captions=[caption])
+        outputs = model(image, captions=[caption])
 
-    prediction_logits = outputs["pred_logits"].cpu().sigmoid()[0]  # prediction_logits.shape = (nq, 256)
-    prediction_boxes = outputs["pred_boxes"].cpu()[0]  # prediction_boxes.shape = (nq, 4)
+    prediction_logits = outputs["pred_logits"].cpu().sigmoid()  # prediction_logits.shape = (b, nq, 256)
+    prediction_boxes = outputs["pred_boxes"].cpu()  # prediction_boxes.shape = (b, nq, 4)
 
-    mask = prediction_logits.max(dim=1)[0] > box_threshold
-    logits = prediction_logits[mask]  # logits.shape = (n, 256)
-    boxes = prediction_boxes[mask]  # boxes.shape = (n, 4)
-
-    tokenizer = model.tokenizer
-    tokenized = tokenizer(caption)
-    
-    if remove_combined:
-        sep_idx = [i for i in range(len(tokenized['input_ids'])) if tokenized['input_ids'][i] in [101, 102, 1012]]
-        
-        phrases = []
-        for logit in logits:
-            max_idx = logit.argmax()
-            insert_idx = bisect.bisect_left(sep_idx, max_idx)
-            right_idx = sep_idx[insert_idx]
-            left_idx = sep_idx[insert_idx - 1]
-            phrases.append(get_phrases_from_posmap(logit > text_threshold, tokenized, tokenizer, left_idx, right_idx).replace('.', ''))
-    else:
-        phrases = [
-            get_phrases_from_posmap(logit > text_threshold, tokenized, tokenizer).replace('.', '')
-            for logit
-            in logits
-        ]
-
-    return boxes, logits.max(dim=1)[0], phrases
+    mask = prediction_logits.max(dim=2)[0] > box_threshold
+    logits = prediction_logits[mask]  # logits.shape = (b, n, 256)
+    boxes = prediction_boxes[mask]  # boxes.shape = (b, n, 4)
+    return boxes, logits.max(dim=2)[0]
 
 
 def annotate(image_source: np.ndarray, boxes: torch.Tensor, logits: torch.Tensor, phrases: List[str]) -> np.ndarray:
@@ -137,10 +114,9 @@ class Model:
 
     def predict_with_caption(
         self,
-        image: np.ndarray,
+        image: torch.Tensor,
         caption: str,
         box_threshold: float = 0.35,
-        text_threshold: float = 0.25
     ) -> Tuple[sv.Detections, List[str]]:
         """
         import cv2
@@ -160,28 +136,26 @@ class Model:
         box_annotator = sv.BoxAnnotator()
         annotated_image = box_annotator.annotate(scene=image, detections=detections, labels=labels)
         """
-        processed_image = Model.preprocess_image(image_bgr=image).to(self.device)
-        boxes, logits, phrases = predict(
+        processed_image = image.to(self.device)
+        boxes, logits = predict(
             model=self.model,
             image=processed_image,
             caption=caption,
             box_threshold=box_threshold,
-            text_threshold=text_threshold, 
             device=self.device)
-        source_h, source_w, _ = image.shape
+        _, source_h, source_w, _ = image.shape
         detections = Model.post_process_result(
             source_h=source_h,
             source_w=source_w,
             boxes=boxes,
             logits=logits)
-        return detections, phrases
+        return detections
 
     def predict_with_classes(
         self,
-        image: np.ndarray,
+        image: torch.Tensor,
         classes: List[str],
         box_threshold: float,
-        text_threshold: float
     ) -> sv.Detections:
         """
         import cv2
@@ -203,22 +177,19 @@ class Model:
         annotated_image = box_annotator.annotate(scene=image, detections=detections)
         """
         caption = ". ".join(classes)
-        processed_image = Model.preprocess_image(image_bgr=image).to(self.device)
-        boxes, logits, phrases = predict(
+        processed_image = image.to(self.device)
+        boxes, logits = predict(
             model=self.model,
             image=processed_image,
             caption=caption,
             box_threshold=box_threshold,
-            text_threshold=text_threshold,
             device=self.device)
-        source_h, source_w, _ = image.shape
+        _, _, source_h, source_w = image.shape
         detections = Model.post_process_result(
             source_h=source_h,
             source_w=source_w,
             boxes=boxes,
             logits=logits)
-        class_id = Model.phrases2classes(phrases=phrases, classes=classes)
-        detections.class_id = class_id
         return detections
 
     @staticmethod
@@ -241,10 +212,10 @@ class Model:
             boxes: torch.Tensor,
             logits: torch.Tensor
     ) -> sv.Detections:
-        boxes = boxes * torch.Tensor([source_w, source_h, source_w, source_h])
-        xyxy = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
-        confidence = logits.numpy()
-        return sv.Detections(xyxy=xyxy, confidence=confidence)
+        boxes = boxes * torch.Tensor([source_w, source_h, source_w, source_h])[None, None, :]
+        xyxys = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
+        confidences = logits.numpy()
+        return [sv.Detections(xyxy=xyxy, confidence=confidence) for xyxy, confidence in zip(xyxys, confidences)]
 
     @staticmethod
     def phrases2classes(phrases: List[str], classes: List[str]) -> np.ndarray:
